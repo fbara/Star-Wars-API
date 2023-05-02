b81344055e08c128e4f01f52246c42aff9aa777b
fbara <fbara1@yahoo.com>
2023-05-02 13:51:19 -0400

    Initial Commit

---
+++ Star Wars API/Star_Wars_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_APIApp.swift
+//  Star Wars API
+//
+//  Created by Frank Bara on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
